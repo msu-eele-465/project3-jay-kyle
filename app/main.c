4974a97e99a32f66e9a410ca60001686fc253d5b
kstopplecamp <49496979+kstopplecamp@users.noreply.github.com>
2025-02-13 16:34:14 -0700

    Heartbeat LED set up

--- app/main.c
+++ app/main.c
@@ -1,3 +1,4 @@
+#include "msp430fr2355.h"
 #include <msp430.h>
 #include <stdbool.h>
 
@@ -6,11 +7,20 @@ int main(void)
     // Stop watchdog timer
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     
+    P1DIR   |= BIT0;
+
     // Disable the GPIO power-on default high-impedance mdoe to activate
     // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
 
+    int i = 0;
+    
     while(true)
     {
+        P1OUT^= BIT0;
+        for(i=0;    i<0xFFFF; i++)
+        {
+
+        }
     }
 }
