05d05468c0b67f6bb9851d1b54f7b873fc247f9c
kstopplecamp <49496979+kstopplecamp@users.noreply.github.com>
2025-02-13 22:17:59 -0700

    Functionality to find the key that was pressed. I havent tested the code yet as i need my keypad

--- app/main.c
+++ app/main.c
@@ -2,6 +2,12 @@
 #include <msp430.h>
 #include <stdbool.h>
 
+    int i = 0;
+    int status = 0;
+    int col = 0;
+ 
+    char key = 'N';        // starts the program at NA until a key gets pressed
+
 int main(void)
 {
     // Stop watchdog timer
@@ -13,14 +19,173 @@ int main(void)
     // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
 
-    int i = 0;
-    
+
     while(true)
     {
         P1OUT^= BIT0;
-        for(i=0;    i<0xFFFF; i++)
+        for(i=0;    i<0xFFFF; i++);
         {
 
         }
     }
+}    /*
+    check_keypad(status){
+        if(status == 0){
+            if(key = code_int_1){
+                status = 1
+            }
+        }
+    } 
+    */
+
+void get_column()
+{
+    int col_1 = (P1IN & BIT6) ? 1 : 0;
+    int col_2 = (P1IN & BIT7) ? 1 : 0;
+    int col_3 = (P2IN & BIT0) ? 1 : 0;
+    int col_4 = (P2IN & BIT1) ? 1 : 0;
+
+    if (col_1) {
+        col = 1;
+    } 
+    
+    else if (col_2) {
+        col = 2;
+    }
+
+    else if (col_3) {
+        col = 3;
+    } 
+
+    else if (col_4) {
+        col = 4;
+    } 
+    
+    else {
+        col = 0;  // No key pressed
+    }
 }
+
+
+void get_key()
+{
+    P3DIR |= BIT6 | BIT7; // Set keypad row pins as outputs
+    P1DIR |= BIT4 | BIT5;
+
+    P3DIR &= BIT6 | BIT7; // clears outputs to start
+    P1DIR &= BIT4 | BIT5;
+
+    // Set column pins as input with pull-down resistors
+    P1DIR &= ~(BIT6 | BIT7); // Set P1.6 and P1.7 as input
+    P1REN |= (BIT6 | BIT7);  // Enable pull-up/down resistors
+    P1OUT &= (BIT6 | BIT7);  // Set as pull-down
+
+    P2DIR &= ~(BIT0 | BIT1); // Set P2.0 and P2.1 as input
+    P2REN |= (BIT0 | BIT1);
+    P2OUT &= (BIT0 | BIT1);
+
+    P3OUT |= BIT6; // Activate first row
+    get_column();  
+
+    switch(col){
+    case 1:
+        key = '1';
+        break;
+        
+    case 2:
+        key = '2';
+        break;
+    
+    case 3:
+        key = '3';
+        break;
+
+    case 4:
+        key = 'A';
+        break;
+
+    case 0:
+        break;
+    }
+    P3OUT &= BIT6;      
+
+    P3OUT |= BIT7; // Activate second row
+    get_column();  
+        
+    switch(col){
+    case 1:
+        key = '4';
+        break;
+        
+    case 2:
+        key = '5';
+        break;
+    
+    case 3:
+        key = '6';
+        break;
+
+    case 4:
+        key = 'B';
+        break;
+
+    case 0:
+        break;
+    }
+    P3OUT &= BIT7; 
+
+
+    P1OUT |= BIT4; // Activate third row
+    get_column();  
+
+    switch(col){
+    case 1:
+        key = '7';
+        break;
+        
+    case 2:
+        key = '8';
+        break;
+    
+    case 3:
+        key = '9';
+        break;
+
+    case 4:
+        key = 'C';
+        break;
+
+    case 0:
+        break;
+    }
+    P1OUT &= BIT4; 
+
+
+    P1OUT |= BIT5; // Activate forth row
+    get_column();  
+
+    switch(col){
+    case 1:
+        key = '*';
+        break;
+        
+    case 2:
+        key = '0';
+        break;
+    
+    case 3:
+        key = '#';
+        break;
+
+    case 4:
+        key = 'D';
+        break;
+
+    case 0:
+        break;
+    }
+    P1OUT &= BIT5; 
+        
+}
+
+
