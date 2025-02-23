4c79a8dc93216bc83a184699eb297393751ca0bd
Jay <jaybirdinrlmt@gmail.com>
2025-02-23 14:46:29 -0700

    clean up editing of merge to main of keypad and led stuff

--- app/main.c
+++ app/main.c
@@ -3,30 +3,88 @@
 #include <msp430.h>
 #include <stdbool.h>
 
+int i = 0;              // heartbeet LED integer
+int j = 1;
 
+int status = 0;         // unlock status of the keypad
+int unlocked = 2;       
+int unlocking = 1;
+int locked = 0;
 
-    int i = 0;              // heartbeet LED integer
-    int status = 0;         // unlock status of the keypad
-    int col = 0;            // variable that marks what columb of the keypad was pressed
-    char code_char_1 = '5';
-    char code_char_2 = '2';
-    char code_char_3 = '9';
-    char code_char_4 = '3';
-    int key_num = 0;        // this variable tracks ammt of correct nombers pressed in password
-    char patern_sel = '0';  // default patern
-    int period = 4;         // this will correspond to 1 second changing the integer period variable by 1 should change the period by .25 s
-    int key_pad_flag = 0;
-    int int_en = 0;         //stops intterupt from flagging after inputs go high
-    int pressed = 0;
-    
+int col = 0;            // variable that marks what columb of the keypad was pressed
+char code_char_1 = '5';
+char code_char_2 = '2';
+char code_char_3 = '9';
+char code_char_4 = '3';
+int key_num = 0;        // this variable tracks ammt of correct nombers pressed in password
+int period = 4;         // this will correspond to 1 second changing the integer period variable by 1 should change the period by .25 s
+int key_pad_flag = 0;
+int int_en = 0;         //stops intterupt from flagging after inputs go high
+int pressed = 0;
 
- 
+char pattern = '-1';     // default patern
+char current_pattern = '-2';   
+char next_pattern = '-3';    
+int pattern1 = 0;                       
+int pattern2 = 0;                           
+int pattern3 = 0;
+int pattern4 = 0;
+int pattern5 = 0;
+int pattern3_step = 0;
+float base_transition_scalar = 1.0;        
+
+#define LED_BAR P3OUT
+
     char key = 'N';         // starts the program at NA until a key gets pressed
 
-int main(void)
-{
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+void init_rgb_led(void) {
+    WDTCTL = WDTPW | WDTHOLD;                    // Stop watchdog timer           
+    PM5CTL0 &= ~LOCKLPM5;                        // Disable High Z mode
+
+    // Set P2.0 (red), P2.1 (green), P2.2 (blue) as outputs for RGB led
+    P2DIR |= (BIT0 | BIT1 | BIT2);  
+    P2OUT &= ~(BIT0 | BIT1 | BIT2); 
+
+    // Configure Timer B3
+    TB3CTL |= (TBSSEL__SMCLK | MC__UP | TBCLR);  // Use SMCLK, up mode, clear
+    TB3CCR0 = 16320;                              // Set PWM period (adjust for desired frequency)
+
+    // Enable and clear interrupts for each color channel
+    TB3CCTL0 |= CCIE;                            // Interrupt for base period
+    TB3CCTL0 &= ~CCIFG; 
+    TB3CCTL1 |= CCIE;                            // Interrupt for Red
+    TB3CCTL1 &= ~CCIFG; 
+    TB3CCTL2 |= CCIE;                            // Interrupt for Green
+    TB3CCTL2 &= ~CCIFG;
+    TB3CCTL3 |= CCIE;                            // Interrupt for Blue
+    TB3CCTL3 &= ~CCIFG;
+
+    __enable_interrupt();                        // enable interrupts
+}
+    
+void init_led_bar(void) {
+    WDTCTL = WDTPW | WDTHOLD;                    // Stop watchdog timer           
+    PM5CTL0 &= ~LOCKLPM5;                        // Disable High Z mode
+
+    // Set P1.0-P1.7 as outputs for led bar
+    P5DIR |= 0b11111111; 
+    P5OUT |= 0b00000000;  
+
+    // Configure Timer B0
+    TB0CTL |= (TBSSEL__ACLK | MC__UP | TBCLR); // Use ACLK, up mode, clear
+    TB0CCR0 = (int)(base_transition_scalar * 32768);
+
+    // Enable and clear interrupts for each color channel
+    TB0CCTL0 |= CCIE;                            // Interrupt for pattern transistions
+    TB0CCTL0 &= ~CCIFG;
+
+    __enable_interrupt();                        // enable interrupts
+}
+
+void init_keypad(void) {
+    WDTCTL = WDTPW | WDTHOLD;                    // Stop watchdog timer           
+    PM5CTL0 &= ~LOCKLPM5;                        // Disable High Z mode
+
     //--set up ports
     P1DIR |=  (BIT4   |   BIT5   |   BIT6   |   BIT7);  // Set keypad row pins as outputs
     P1OUT |=  (BIT4   |   BIT5   |   BIT6   |   BIT7);  // sets output high to start
@@ -47,7 +105,13 @@ int main(void)
     // Disable the GPIO power-on default high-impedance mdoe to activate
     // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
+}
 
+int main(void)
+{
+    init_rgb_led();
+    init_led_bar();
+    init_keypad();
 
     while(1){
         pressed = (P2IN & 0b00001111);
@@ -65,7 +129,6 @@ int main(void)
         }
     }
     return 0;
-    
 }    
 
 void get_column()
@@ -209,7 +272,7 @@ void check_keypad(){
         get_key();
         if(status == 2){
             if(key =='0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7'){
-                patern_sel = key;
+                pattern = key;
             }
             if(key == 'A'){
                 if(period > 1){
@@ -278,111 +341,29 @@ void check_keypad(){
         key_pad_flag == 0;
     } 
 
-
-/*
-//------Interrupt Service Routines
-#pragma  vector = PORT1_VECTOR
-__interrupt  void ISR_PORT1_key(void){
-    if(int_en == 1){
-        key_pad_flag = 1; 
-
-    }                       
-    P1IFG &= ~(BIT4   |   BIT5   |   BIT6   |   BIT7);      // clears interrupt flag, could pottentialy set up so 
-                                                            // i could get rid of get column function
-
-}
-*/
-=======
-int unlocked = 0;
-int unlocking = 1;
-int locked = 2;
-int status = -1;
-int pattern = -1;
-int pattern1 = 0;                       
-int pattern2 = 0;                           
-int pattern3 = 0;
-int pattern4 = 0;
-int pattern5 = 0;
-int pattern3_step = 0;
-int current_pattern = -1;   
-int next_pattern = -1;                        
-float base_transition_scalar = 1.0;        
-int i = 1;
-
-#define LED_BAR P5OUT
-
-void init(void) {
-    WDTCTL = WDTPW | WDTHOLD;                    // Stop watchdog timer           
-    PM5CTL0 &= ~LOCKLPM5;                        // Disable High Z mode
-
-    // Set P2.0 (red), P2.1 (green), P2.2 (blue) as outputs for RGB led
-    P2DIR |= (BIT0 | BIT1 | BIT2);  
-    P2OUT &= ~(BIT0 | BIT1 | BIT2); 
-
-    // Set P1.0-P1.7 as outputs for led bar
-    P5DIR |= 0b11111111; 
-    P5OUT |= 0b00000000;  
-
-    // Configure Timer B0
-    TB0CTL |= (TBSSEL__ACLK | MC__UP | TBCLR); // Use ACLK, up mode, clear
-    TB0CCR0 = (int)(base_transition_scalar * 32768);
-
-    // Configure Timer B3
-    TB3CTL |= (TBSSEL__SMCLK | MC__UP | TBCLR);  // Use SMCLK, up mode, clear
-    TB3CCR0 = 16320;                              // Set PWM period (adjust for desired frequency)
-
-    // Enable and clear interrupts for each color channel
-    TB0CCTL0 |= CCIE;                            // Interrupt for pattern transistions
-    TB0CCTL0 &= ~CCIFG;
-    TB3CCTL0 |= CCIE;                            // Interrupt for base period
-    TB3CCTL0 &= ~CCIFG; 
-    TB3CCTL1 |= CCIE;                            // Interrupt for Red
-    TB3CCTL1 &= ~CCIFG; 
-    TB3CCTL2 |= CCIE;                            // Interrupt for Green
-    TB3CCTL2 &= ~CCIFG;
-    TB3CCTL3 |= CCIE;                            // Interrupt for Blue
-    TB3CCTL3 &= ~CCIFG;
-
-    __enable_interrupt();                        // enable interrupts
-}
-
-int main(void)
-{
-    init();
-
-    while(1) {
-        status = unlocked;
-        pattern = 5;
-        if (i == 1) {
-            update_leds(status, pattern);
-        }
-        i = 2;
-    }
-}
-
-void update_leds(int status, int pattern) {
+void update_leds(int status, char pattern) {
     update_rgb_led(status, pattern);
     update_led_bar(status, pattern);
 }
 
-void update_rgb_led(int status, int pattern) {
+void update_rgb_led(int status, char pattern) {
 
     if (status == unlocked) {                
-        if (pattern == 0) {
+        if (pattern == '0') {
             set_rgb_led_pwm(1,254,1);   // green
-        } else if (pattern == 1) {
+        } else if (pattern == '1') {
             set_rgb_led_pwm(75,1,130);  // purple
-        } else if (pattern == 2) {
+        } else if (pattern == '2') {
             set_rgb_led_pwm(254,50,254);  // pink
-        } else if (pattern == 3) {
+        } else if (pattern == '3') {
             set_rgb_led_pwm(254,254,254);  // white
-        } else if (pattern == 4) {
+        } else if (pattern == '4') {
             set_rgb_led_pwm(150,75,1);     // yellow    
-        } else if (pattern == 5) {
+        } else if (pattern == '5') {
             set_rgb_led_pwm(10,50,1);              // unknown
-        } else if (pattern == 6) {
+        } else if (pattern == '6') {
             set_rgb_led_pwm(200,150,35);              // unknown
-        } else if (pattern == 7) {
+        } else if (pattern == '7') {
             set_rgb_led_pwm(35,200,150);              // unknown
         } else {
             set_rgb_led_pwm(254,1,1);     // red
@@ -402,22 +383,22 @@ void set_rgb_led_pwm(int red, int green, int blue) {
     TB3CCR3 = blue*64;  // Blue brightness
 }
 
-void update_led_bar(int status, int pattern) {
+void update_led_bar(int status, _MSVC_EXECUTION_CHARACTER_SET pattern) {
 
     if (status == unlocked) {                   
         next_pattern = pattern;
 
         if (next_pattern == current_pattern) {
-            if (pattern == 1) {
+            if (pattern == '1') {
                 pattern1 = 0b10101010;
-            } else if (pattern == 2) {
+            } else if (pattern == '2') {
                 pattern2 = 0b00000000;
-            } else if (pattern == 3) {
+            } else if (pattern == '3') {
                 pattern3 = 0b00011000;
                 pattern3_step = 0;
-            } else if (pattern == 4) {
+            } else if (pattern == '4') {
                 pattern4 = 0b11111111;
-            } else if (pattern == 5) {
+            } else if (pattern == '5') {
                 pattern5 = 0b00000001;
             }
         }
@@ -428,9 +409,9 @@ void update_led_bar(int status, int pattern) {
 #pragma vector = TIMER0_B0_VECTOR
 __interrupt void Pattern_Transition_ISR(void) {
     if (status == unlocked) {
-        if (pattern == 0) {
+        if (pattern == '0') {
             LED_BAR = 0b10101010;
-        } else if (pattern == 1) {
+        } else if (pattern == '1') {
             base_transition_scalar = 1.0;
             if (pattern1 == 0b01010101) { 
                 pattern1 = 0b10101010;       
@@ -438,7 +419,7 @@ __interrupt void Pattern_Transition_ISR(void) {
                 pattern1 = 0b01010101;
             }
             LED_BAR = pattern1;
-        } else if (pattern == 2) {
+        } else if (pattern == '2') {
             base_transition_scalar = 0.5;
             if (pattern2 != 0b11111111) {
                 pattern2 = pattern2 + 1;
@@ -446,7 +427,7 @@ __interrupt void Pattern_Transition_ISR(void) {
                 pattern2 = 0b00000000;
             }
             LED_BAR = pattern2;
-        } else if (pattern == 3) {
+        } else if (pattern == '3') {
             base_transition_scalar = 0.5;
             if (pattern3_step == 0) {
                 pattern3 = 0b00100100;
@@ -468,7 +449,7 @@ __interrupt void Pattern_Transition_ISR(void) {
                 pattern3_step = 0;
             }
             LED_BAR = pattern3;
-        } else if (pattern == 4) {
+        } else if (pattern == '4') {
             base_transition_scalar = 0.25;
             if (pattern4 != 0b00000000) {
                 pattern4 = pattern4 - 1;
@@ -476,7 +457,7 @@ __interrupt void Pattern_Transition_ISR(void) {
                 pattern4 = 0b11111111;
             }
             LED_BAR = pattern4;
-        } else if (pattern == 5) {
+        } else if (pattern == '5') {
             base_transition_scalar = 1.5;
             if (pattern5 != 0b10000000) {
                 pattern5 = pattern5 << 1;       // FIX ME: Shift left operation
